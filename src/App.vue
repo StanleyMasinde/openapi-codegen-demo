59766aa89364cc2f8e1416d3c41e3418fe78367d
Stanley Masinde <stanmasinde@gmail.com>
2023-08-18 11:10:59 +0300

    Add a simple vue app for the demo

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { Ref, onMounted, ref } from 'vue'
+import { DefaultApi, Post } from '../generated/index'
+
+const postsAPI = new DefaultApi()
+const posts: Ref<Post[]> = ref([])
+
+
+onMounted(async () => {
+  posts.value = await postsAPI.getAllPosts()
+  console.log(posts);
+})
+
+</script>
+
+<template>
+  <main>
+    <div class="card" v-for="post in posts">
+      <h3>{{ post.title }}</h3>
+      <p>{{ post.body }}</p>
+    </div>
+  </main>
+</template>
+<style scoped></style>
